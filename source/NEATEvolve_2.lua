3fe84dcbc5e491b8fdfb99376e086b905b59a138
dustytrash <Lionelbergen@live.com>
2021-02-16 17:59:57 -0600

    restructuring

--- source/NEATEvolve_2.lua
+++ source/NEATEvolve_2.lua
@@ -18,6 +18,7 @@ ButtonNames = {
 }
 
 FileUtil = require('util/FileUtil')
+MLAIGaming = require('MLAIGaming/MLAIGaming')
 
 if gameinfo.getromname() ~= romGameName then
 	error('Unsupported Game Rom! Please play rom: ' .. romGameName .. ' Rom currently is: ' .. gameinfo.getromname())
@@ -52,35 +53,7 @@ MaxNodes = 1000000
 
 -- Load the saved slot. This savefile needs to be set manually before this program starts
 function loadSavedMarioGame(fileLocation)
-	local currentDir = FileUtil.getCurrentDirectory()
-	local fullFilePath = currentDir .. "\\" ..fileLocation
-	FileUtil.validateFilePath(fullFilePath)
-	
-	savestate.load(fullFilePath)
-	console.log('loaded save game from file.')
-end
-
-function loadLatestBackup()
-	local listOfAllPoolFiles = FileUtil.scandir(poolSavesFolder)
-	local latestBackupNumber = -1
-	local latestBackupFile
-
-	for key, value in pairs(listOfAllPoolFiles) do
-		res = value.match(value, [[backup.(%d)]])
-		
-		if res ~= nil and latestBackupNumber < tonumber(res) then
-			latestBackupNumber = tonumber(res)
-			latestBackupFile = value
-		end
-	end
-	
-	if latestBackupFile ~= null then
-		console.log('attempting to load file for pool...: ' .. latestBackupFile)
-		loadFile(poolSavesFolder .. latestBackupFile)
-		console.log('loaded backfile: ' .. latestBackupFile)
-	else 
-		console.log('No backup file to load from. looked in directory: ' .. poolSavesFolder .. ' will continue new program')
-	end
+	MLAIGaming.loadSavedGame(fileLocation)
 end
 
 function saveNewBackup(poolGeneration)
@@ -1140,7 +1113,8 @@ hideBanner = forms.checkbox(form, "Hide Banner", 5, 190)
 
 console.log('beginning program....')
 
-loadLatestBackup()
+-- Load the latest .pool file
+MLAIGaming.loadLatestBackup(poolSavesFolder)
 
 while true do
 	local backgroundColor = 0xD0FFFFFF
